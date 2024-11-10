de8af3464d8d06a8b1f35dd692fa60ecf9310b3f
ocavue <ocavue@gmail.com>
2024-11-10 22:29:35 +1100

    add example

--- examples/node/main.js
+++ examples/node/main.js
@@ -1,26 +1,45 @@
 const { loadOpenCV } = require("@opencvjs/node");
-const Jimp = require("jimp");
+const { Canvas, createCanvas, Image, ImageData, loadImage } = require("canvas");
+const { JSDOM } = require("jsdom");
+const { writeFileSync } = require("fs");
 
 async function main() {
+  // before loading opencv.js we emulate a minimal HTML DOM. See the function declaration below.
+  installDOM();
+
   const cv = await loadOpenCV();
 
-  // Load local image file with jimp. It supports jpg, png, bmp, tiff and gif:
-  const jimpSrc = await Jimp.read("./assets/Lenna.jpg");
-  const src = cv.imread(jimpSrc.bitmap);
+  // Convert an image file to an object compatible with
+  // HTML DOM Image and therefore with cv.imread()
+  const image = await loadImage("Lenna.jpg");
 
-  // Convert the image to grayscale
-  const tmp = new cv.Mat();
-  cv.cvtColor(src, tmp, cv.COLOR_RGBA2GRAY, 0);
+  // Read the image and convert it to grayscale
+  const src = cv.imread(image);
   const dst = new cv.Mat();
-  cv.cvtColor(tmp, dst, cv.COLOR_GRAY2RGBA, 0);
+  cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY, 0);
 
-  // Save the grayscale image
-  cv.imwrite("./assets/Lenna_gray.jpg", dst);
+  // Create an object compatible with HTMLCanvasElement
+  const canvas = createCanvas(256, 256);
+  cv.imshow(canvas, dst);
+  writeFileSync("output.jpg", canvas.toBuffer("image/jpeg"));
 
   // Release the memory
   src.delete();
-  tmp.delete();
   dst.delete();
+}
+
+// Using jsdom and node-canvas we define some global variables to emulate HTML
+// DOM. Although a complete emulation can be archived, here we only define those
+// globals used by cv.imread() and cv.imshow().
+function installDOM() {
+  const dom = new JSDOM();
+  global.document = dom.window.document;
+
+  // The rest enables DOM image and canvas and is provided by node-canvas
+  global.Image = Image;
+  global.HTMLCanvasElement = Canvas;
+  global.ImageData = ImageData;
+  global.HTMLImageElement = Image;
 }
 
 main();
--- packages/node/lib/index.js
+++ packages/node/lib/index.js
@@ -1,12 +1,18 @@
-const cv = require("./opencv_js.js");
 
 let promise;
 
-function loadOpenCV() {
-  if (!promise) {
-    promise = cv();
-  }
-  return promise;
+async function loadOpenCV() {
+  const cv = require("./opencv_js.js");
+
+  console.log("cv::", cv);
+
+  const result = cv();
+  return result;
+
+  // if (!promise) {
+  //   promise = cv();
+  // }
+  // return promise;
 }
 
 module.exports = { loadOpenCV };
