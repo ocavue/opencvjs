40232a46672110b17baffb2a522f4f16231a04ad
ocavue <ocavue@gmail.com>
2024-11-10 19:25:01 +1100

    fix

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex 
 
-EMSCRIPTEN_SDK_VERSION=3.1.71
+EMSCRIPTEN_EMSDK_VERSION=3.1.71
 OPENCV_VERSION=4.10.0
 
 cd $(dirname $0)
@@ -15,7 +15,7 @@ git clone https://github.com/opencv/opencv.git || true
 cd opencv
 git checkout ${OPENCV_VERSION}
 
-docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk:$EMSCRIPTEN_SDK_VERSION emcmake python3 ./platforms/js/build_js.py build_js_web --build_flags="-s ENVIRONMENT=web -s EXPORT_ES6=1 -s MODULARIZE=1"
+docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk:$EMSCRIPTEN_EMSDK_VERSION emcmake python3 ./platforms/js/build_js.py build_js_web --build_flags="-s ENVIRONMENT=web -s EXPORT_ES6=1 -s MODULARIZE=1"
 
 cd $ROOT_DIR
 # cp build/opencv/build_js/bin/opencv_js.js dist/opencv-default.js
