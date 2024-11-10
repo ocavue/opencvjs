8970b0cfd0b864c681ab9d2c7abb1f754146ad78
ocavue <ocavue@gmail.com>
2024-11-10 22:31:24 +1100

    install

--- packages/node/lib/index.js
+++ packages/node/lib/index.js
@@ -1,18 +1,12 @@
+const cv = require("./opencv_js.js");
 
 let promise;
 
 async function loadOpenCV() {
-  const cv = require("./opencv_js.js");
-
-  console.log("cv::", cv);
-
-  const result = cv();
-  return result;
-
-  // if (!promise) {
-  //   promise = cv();
-  // }
-  // return promise;
+  if (!promise) {
+    promise = cv();
+  }
+  return promise;
 }
 
 module.exports = { loadOpenCV };
